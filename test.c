4e629370ebc11a4ee4fb7d21e75bd6797e6d19b5
gilgamesh-322 <79505793+gilgamesh-322@users.noreply.github.com>
2021-02-23 16:58:13 +0800

    memory
    add memory

--- test.c
+++ test.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
 int main(void)
 {
+  char *p = (char *)malloc(sizeof(char));
   printf("helloworld\r\n");
   printf("exit\r\n");
-  
+  free(p);
   return 0;
 }
